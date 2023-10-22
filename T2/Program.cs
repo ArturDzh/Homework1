aba1e600b05a8cb5ee10f574b2d8ac1607fbc06a
ArturDzh <arturdzhussoev@gmail.com>
2023-10-22 16:45:23 +0300

    h2

---
+++ T2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите три числа, и узнайте, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число и нажмите ENTER: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число и нажмите ENTER: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число и нажмите ENTER: ");
+int num3 = int.Parse(Console.ReadLine());
+int maxN = 0;
+if (num1 > maxN) maxN = num1;
+if (num2 > maxN) maxN = num2;
+if (num3 > maxN) maxN = num3;
+Console.Write("Максимальное число " + maxN);
---
+++ T4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число- ");
+int num = int.Parse(Console.ReadLine());
+int s = 2;
+while (num >= s)
+{
+    Console.Write(s + " ");
+    s = s + 2;
+}
